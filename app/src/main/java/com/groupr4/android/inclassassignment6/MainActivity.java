8fdfd9a472e8cc9f95e89f7b6aa81fe287bca1e9
Adwait <atathe@uncc.edu>
2018-10-15 20:26:55 -0400

    xyz

--- app/src/main/java/com/groupr4/android/inclassassignment6/MainActivity.java
+++ app/src/main/java/com/groupr4/android/inclassassignment6/MainActivity.java
@@ -9,7 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
 
         super.onCreate(savedInstanceState);
@@ -34,6 +43,31 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+
+                RequestBody formBody = new FormBody.Builder()
+                        .add("email","ab" )
+                        .add("email","fd" )
+                        .build();
+                Request request = new Request.Builder()
+                        .url("http://ec2-18-234-222-229.compute-1.amazonaws.com/api/login")
+                        .post(formBody)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+
+
+                    }
+                });
+
 
 
 
